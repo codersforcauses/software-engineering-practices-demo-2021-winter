fa13ad43c253d98792b8f6545c8377cc368ad542
Sep Kimiaei <62089596+heyseppy@users.noreply.github.com>
2021-07-03 11:38:58 +0800

    done multiplication (#14)

--- src/utils/operations.py
+++ src/utils/operations.py
@@ -22,13 +22,9 @@ def subtraction(num1, num2):
 
 
 def multiplication(num1, num2):
-    """
-    This function will perform a multiplication operation on num1 by num2 
-    
-    Note: This function is very simplified... but for the purpose of this exercise
-    Imagine that you are building a very complex function, and you want to be able to
-    work with multiple people.
-    """
+    print(num1*num2)
+    if (num1 == 0):
+        print("hehehehe")
     # Add code here
     pass
 
