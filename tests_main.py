bfa54608f236db697be5aca80a7e3fd39d8bf679
APM246 <apm@APM-Laptop.localdomain>
2021-07-17 12:18:59 +0800

    re-added blank line fix

--- tests_main.py
+++ tests_main.py
@@ -1,11 +1,11 @@
 from main import perform_operation_with_two_nums
 
+
 def test_perform_operation_with_two_nums():
     """
     GIVEN: Two numbers and an operation
     WHEN:  passed in to the function perform_operation_with_two_nums
     THEN:  the resulting is correct based on mathematical rules
-
 
     Note: The difference between the tests here is to make sure that the bigger function works
     in orchestration of operation
